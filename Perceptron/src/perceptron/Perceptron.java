e23b57d05e99ca8556925b93f8715c172305cce0
Ayamahdy2000 <aya.mahdy512000@gmail.com>
2020-04-14 20:20:14 +0200

    Second Update

--- Perceptron/src/perceptron/Perceptron.java
+++ Perceptron/src/perceptron/Perceptron.java
@@ -3,7 +3,7 @@ package perceptron;
 import java.util.*;
 
 public class Perceptron {
-    
+
     public static void main(String[] args) {
         int iuputNumber, rowNumber, colNumber;
         Scanner in = null;
@@ -14,44 +14,44 @@ public class Perceptron {
         rowNumber = in.nextInt();
         System.out.println("Enter the number of columns of the matrix:");
         colNumber = in.nextInt();
-        int P[][] = new int[rowNumber][colNumber*iuputNumber];
-        
-            System.out.println("Enter the elements of the matrix");
-            for (int j = 0; j < rowNumber; j++) {
-                for (int x = 0; x < colNumber*iuputNumber; x++) {
-                    P[j][x] = in.nextInt();
-                }
-            }
+        int P[][] = new int[rowNumber][colNumber * iuputNumber];
 
-           for (int x = 0; x < colNumber*iuputNumber; x++)  {
-                 for (int j = 0; j < rowNumber; j++) {
-                    System.out.print(P[j][x] + "  ");
-                   
-                }
-                System.out.println("");
+        System.out.println("Enter the elements of the matrix");
+        for (int x = 0; x < colNumber * iuputNumber; x++) {
+            for (int j = 0; j < rowNumber; j++) {
+                P[j][x] = in.nextInt();
             }
-        int  rowTarget, colTarget;
-       
+        }
+
+        for (int j = 0; j < rowNumber; j++) {
+            for (int x = 0; x < colNumber * iuputNumber; x++) {
+                System.out.print(P[j][x] + "  ");
+
+            }
+            System.out.println("");
+        }
+        int rowTarget, colTarget;
+
         in = new Scanner(System.in);
         System.out.println("Enter the number of rows of the matrix:");
         rowNumber = in.nextInt();
         System.out.println("Enter the number of columns of the matrix:");
         colNumber = in.nextInt();
-        int T[][] = new int[rowNumber][colNumber*iuputNumber];
-        
-            System.out.println("Enter the elements of the matrix");
-            for (int j = 0; j < rowNumber; j++) {
-                for (int x = 0; x < colNumber*iuputNumber; x++) {
-                    T[j][x] = in.nextInt();
-                }
-            }
+        int T[][] = new int[rowNumber][colNumber * iuputNumber];
 
-                for (int j = 0; j < rowNumber; j++){
-             for (int x = 0; x < colNumber*iuputNumber; x++)  {
-                    System.out.print(T[j][x] + "  ");
-                   
-                }
-                System.out.println("");
+        System.out.println("Enter the elements of the matrix");
+        for (int x = 0; x < colNumber * iuputNumber; x++) {
+            for (int j = 0; j < rowNumber; j++) {
+                T[j][x] = in.nextInt();
             }
+        }
+
+        for (int j = 0; j < rowNumber; j++) {
+            for (int x = 0; x < colNumber * iuputNumber; x++) {
+                System.out.print(T[j][x] + "  ");
+
+            }
+            System.out.println("");
+        }
     }
 }
